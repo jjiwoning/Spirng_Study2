77da66ffefab23a84f914fba595289e316093ca0
JJiWoNing <jjiwoning12@gmail.com>
2022-04-14 01:57:29 +0900

    4/14_Study

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,19 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// 설정 정보 (구성 정보)를 나타낸다. 어플리케이션의 어떻게 구성되는지를 담당하는 코드
+@Configuration
 public class AppConfig {
 
+    // 이렇게 Bean으로 등록해두면 스프링 컨테이너에 등록이 된다.
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
         //생성자를 통해서 구현체를 외부에서 주입한다. 생성자 주입
         //리팩터링 : 커맨드 옵션 m
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
@@ -29,6 +37,7 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 
         //return new FixDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,26 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     // 순수한 자바 코드로 테스트 개발 -> 한계가 있다.
     // junit을 쓰는게 좋다
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig를 사용하는 버전
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+
+        //스프링을 사용하는 버전
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 스프링은 항상 이걸로 시작한다. -> 스프링 컨테이너라고 보면 된다. -> 모든 객체들을 관리해주는 역할 수행
+        // AppConfig.class -> 클래스의 인스턴스가 아닌 타입 자체를 넘겨야되기 때문에
+        // new -> 초기화해서 클래스 내 멤버들 접근 가능
+        // .class -> 클래스 타입으로 접근하여 클래스의 속성, 멤버에 대한 정보에 접근 가능 -> 리플렉션이라고 한다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 첫 매개변수는 꺼내려고 하는 메서드의 이름, 두 번째 매개변수는 반환 타입
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
