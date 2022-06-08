dfc137b2a08d772619b7d2709fa7ff22fffeb45f
JJiWoNing <jjiwoning12@gmail.com>
2022-06-08 20:51:04 +0900

    6/8_Study

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -18,8 +18,9 @@ public class OrderServiceImpl implements OrderService{
     // -> DIP , OCP 위반 -> 인터페이스에만 의존하게 다시 설계해야된다.
     private final DiscountPolicy discountPolicy; // 이렇게 변경을 해주면 된다.
     // 구현체가 없어서 실제로 실행하면 null pointer exception 발생 -> 누군가 구현 객체 주입 해줘야된다.
+    // final -> 객체에 값이 무조건 있어야 된다 라는 의미. -> 생성자에 값을 무조건 넣어줘라
 
-    @Autowired // 생성자 위에다 하면 된다.
+    @Autowired // 생성자 위에다 하면 된다. + 생성자가 1개만 있다면 Autowired를 생략해도 자동 주입이 된다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
